716db3e427421fce1533abebfa5547a6d76c4035
Luca Sbardella <luca@quantmind.com>
2017-11-01 09:32:31 +0000

    pages

--- dev/publish.sh
+++
@@ -1,7 +0,0 @@
-cd .. && git clone git@github.com:quantmind/topo-regions.git gh-pages
-cd gh-pages && git checkout -b gh-pages origin/gh-pages
-rm -rf *
-cp -a ../topo-regions/website/. .
-git add * && git commit -a -m "new release" && git push origin gh-pages
-cd .. && rm -rf gh-pages
-cd topo-regions
